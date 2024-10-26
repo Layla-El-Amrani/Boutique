0bd81d794c824eab0725ec76849cc3572bd0295b
Layla-El-Amrani <160767348+Layla-El-Amrani@users.noreply.github.com>
2024-10-26 23:04:34 +0100

    Update RouterMiddlware.jsx

--- src/RouterMiddlware.jsx
+++ src/RouterMiddlware.jsx
@@ -10,15 +10,15 @@ import AddProduct from './Component/AddProduct';
 export default function RouterMiddlware({ setCount }) {
   const [cartItem, setCartItem] = useState([]);
   const [products, setProducts] = useState([
-    { id: 1, name: "Montre Classique", price: "$150.00", img: "/image/4.jpg" },
-    { id: 2, name: "Montre Sportive", price: "$200.00", img: "/image/1.avif" },
-    { id: 3, name: "Montre de Luxe", price: "$800.00", img: "/image/5.jpg" },
-    { id: 4, name: "Montre Étanche", price: "$120.00", img: "/image/2.avif" },
-    { id: 5, name: "Montre Minimaliste", price: "$100.00", img: "/image/4.avif" },
-    { id: 6, name: "Montre Connectée", price: "$250.00", img: "/image/4.jpeg" },
-    { id: 7, name: "Montre en Bois", price: "$130.00", img: "/image/4.jpg" },
-    { id: 8, name: "Montre Automatique", price: "$500.00", img: "/image/5.jpg" },
-    { id: 9, name: "Montre Vintage", price: "$300.00", img: "/image/6.jpg" },
+    { id: 1, name: "Montre Classique", price: "$150.00", img: "Boutique/image/4.jpg" },
+    { id: 2, name: "Montre Sportive", price: "$200.00", img: "Boutique/image/1.avif" },
+    { id: 3, name: "Montre de Luxe", price: "$800.00", img: "Boutique/image/5.jpg" },
+    { id: 4, name: "Montre Étanche", price: "$120.00", img: "Boutique/image/2.avif" },
+    { id: 5, name: "Montre Minimaliste", price: "$100.00", img: "Boutique/image/4.avif" },
+    { id: 6, name: "Montre Connectée", price: "$250.00", img: "Boutique/image/4.jpeg" },
+    { id: 7, name: "Montre en Bois", price: "$130.00", img: "Boutique/image/4.jpg" },
+    { id: 8, name: "Montre Automatique", price: "$500.00", img: "Boutique/image/5.jpg" },
+    { id: 9, name: "Montre Vintage", price: "$300.00", img: "Boutique/image/6.jpg" },
   ]);
 
   const handleAddProduct = (formData) => {
